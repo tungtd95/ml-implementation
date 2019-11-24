e3be9dd6536f8f6f4250d5869cb2af42af8468c3
TungVD <v.tungvd2@vinid.net>
2019-11-24 17:10:11 +0700

    update README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Machine Learning Algorithm Implementation
 
 - This project is for learning purpose. For more study material, you could refer to Machine Learning Course by Stanford University (https://www.coursera.org/learn/machine-learning)
-- Dependency: Python 3.8, matplotlib
+- Dependency: Python 3.8, matplotlib, numpy
--- gradient_descent_linear_regression_sample.py
+++ gradient_descent_linear_regression_sample.py
@@ -1,6 +1,6 @@
-import gradient_descent_linear_regression as gradient_descent_linear_regression
+import gradient_descent_linear_regression as gd
 
 training_data = [(1, 2), (1.5, 2.5), (2.5, 4), (3, 3), (3, 4), (4, 3.9), (4.1, 5), (5, 4), (6, 5.9), (6, 5), (6.5, 7),
                  (7, 6), (8, 7), (8.1, 9), (5.5, 5), (6.5, 7), (7, 5), (7.5, 6), (8, 6.5)]
 
-gradient_descent_linear_regression.train(training_data=training_data, alpha=0.006123)
+gd.train(training_data=training_data, alpha=0.006123)
